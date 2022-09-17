417987dc786452849d23340bafa2c4fb56289cd2
Akim <gentilakili98@gmail.com>
2022-09-17 12:27:08 +0200

    OK

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:formation_client/response/responsive.dart';
 
+import '../constants.dart';
 import 'components/card_cours.dart';
 
 class Home extends StatefulWidget {
@@ -17,16 +20,64 @@ class _StateBody extends State<Home> {
       // backgroundColor: Colors.blue,
       body: Column(
         children: [
-      Expanded(
-        child: GridView.builder(
-            itemCount: 10,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount:
-                    (orientation == Orientation.portrait) ? 2 : 3),
-            itemBuilder: (context, index) {
-              return CardCours();
-            }),
-      )
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .3,
+            color: primaryColor,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Votre avenir commence ici",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "Apprenez à apprendre, découvrez les compétences de demain, et prenez votre carrière en main.",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      SvgPicture.asset(
+                        "assets/images/reading.svg",
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width * .2,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                itemCount: 10,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: (orientation == Orientation.portrait)
+                        ? 2
+                        : Responsive.isDesktop(context)
+                            ? 3
+                            : Responsive.isTablet(context)
+                                ? 2
+                                : 1),
+                itemBuilder: (context, index) {
+                  return CardCours();
+                }),
+          )
         ],
       ),
     );
